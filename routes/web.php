52f5ad153aca11e2f37604f2ac9e6b6506c0dffd
Jason Kwok <git@jasonhk.dev>
2025-12-17 17:44:19 +0000

    Added /logout route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Volt::route('/', 'users.index');
+
+Route::get("/logout", function ()
+{
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect("/");
+});
