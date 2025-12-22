0eca631a719e1ad5397669ddf81d08047b362175
Jason Kwok <git@jasonhk.dev>
2025-12-22 17:16:12 +0000

    Removed unused code

--- resources/views/livewire/brand.blade.php
+++ resources/views/livewire/brand.blade.php
@@ -10,10 +10,6 @@ new class extends Component
 
 <div class="brand font-bold text-primary dark:text-neutral-content text-2xl sm:ps-4 {{ $class }}">
     <a href="/" wire:navigate>
-        <!-- <picture>
-            <source srcset="/logo-dark.svg" media="(prefers-color-scheme: dark)" />
-            <img src="/logo.svg" class="inline-block w-25 h-auto align-bottom" />
-        </picture> -->
         <img src="/logo.svg" class="inline-block dark:hidden w-25 h-auto align-bottom" />
         <img src="/logo-dark.svg" class="hidden dark:inline-block w-25 h-auto align-bottom" />
         MyPortal
