c1d4ac2e6e70940c8739eba7ce6fee68594f2d97
Jason Kwok <git@jasonhk.dev>
2025-12-30 18:29:29 +0000

    Removed link

--- resources/views/livewire/brand.blade.php
+++ resources/views/livewire/brand.blade.php
@@ -8,10 +8,8 @@ new class extends Component
     use HasCssClassAttribute;
 }; ?>
 
-<div class="brand font-bold text-primary dark:text-neutral-content text-2xl sm:ps-4 {{ $class }}">
-    <a href="/" wire:navigate>
-        <img src="/logo.svg" class="inline-block dark:hidden w-25 h-auto align-bottom" />
-        <img src="/logo-dark.svg" class="hidden dark:inline-block w-25 h-auto align-bottom" />
-        MyPortal
-    </a>
+<div class="brand font-bold text-primary dark:text-neutral-content text-2xl sm:ps-4 select-none {{ $class }}">
+    <img src="/logo.svg" alt="VTC" class="inline-block dark:hidden w-25 h-17 align-bottom" />
+    <img src="/logo-dark.svg" alt="VTC" class="hidden dark:inline-block w-25 h-17 align-bottom" />
+    MyPortal
 </div>
