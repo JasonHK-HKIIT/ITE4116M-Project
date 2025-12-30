a74c06e76c59826fb583b7bbcfd46c5f5b759684
Jason Kwok <git@jasonhk.dev>
2025-12-30 18:30:03 +0000

    Preloaded SVGs

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ isset($title) ? $title . " :: " . config("app.name") : config("app.name") }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="preload" href="/logo.svg" as="image" media="(prefers-color-scheme: light)" />
+    <link rel="preload" href="/logo-dark.svg" as="image" media="(prefers-color-scheme: dark)" />
     @vite(["resources/css/app.css", "resources/js/app.js"])
 </head>
 <body class="min-h-screen font-sans antialiased bg-base-200">
