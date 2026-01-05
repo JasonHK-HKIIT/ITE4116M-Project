78d93555a9d57f64c1e87f00c9bb8b213c3ef052
Jason Kwok <git@jasonhk.dev>
2026-01-05 19:05:18 +0000

    Add content purification to NewsArticleTranslation model

--- app/Models/NewsArticleTranslation.php
+++ app/Models/NewsArticleTranslation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Stevebauman\Purify\Casts\PurifyHtmlOnGet;
 
 class NewsArticleTranslation extends Model
 {
@@ -13,4 +14,11 @@ class NewsArticleTranslation extends Model
     public $timestamps = false;
 
     protected $fillable = ['title', 'content'];
+
+    protected function casts()
+    {
+        return [
+            'content' => PurifyHtmlOnGet::class,
+        ];
+    }
 }
