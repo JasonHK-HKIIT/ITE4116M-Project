e7380a89f7760120e14a281bed08046b38221d45
Jason Kwok <git@jasonhk.dev>
2026-01-05 18:57:31 +0000

    Refactor news article edit component to use Validate attributes and support multilingual content

--- resources/views/pages/dashboard/news/edit.blade.php
+++ resources/views/pages/dashboard/news/edit.blade.php
@@ -1,69 +1,94 @@
 <?php
 
+use App\Helpers\LocalesHelper;
 use App\Models\NewsArticle;
 use Livewire\Attributes\Layout;
-use Livewire\Attributes\Rule;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 new
 #[Layout('layouts::dashboard')]
 class extends Component
 {
+    public string $selectedLanguage = 'en';
+
+    public bool $exists = false;
+
     public NewsArticle $article;
 
-    public ?int $id;
+    #[Validate('required')]
+    public array $title = [];
 
-    #[Rule(['required'])]
-    public string $title = '';
-
-    #[Rule(['required'])]
+    #[Validate(['required', 'alpha_dash:ascii', 'max:127'])]
     public string $slug = '';
 
-    public function mount(?NewsArticle $article)
+    #[Validate('required')]
+    public array $content = [];
+
+    protected function rules(): array
     {
+        return array_merge(
+            LocalesHelper::getRules('title', ['required', 'max:255']),
+            LocalesHelper::getRules('content', ['required', 'max:16777215']),
+        );
+    }
+
+    protected function validationAttributes()
+    {
+        return array_merge(
+            LocalesHelper::getValidationAttributes('title'),
+            LocalesHelper::getValidationAttributes('content'),
+        );
+    }
+
+    public function mount(NewsArticle $article)
+    {
+        $this->exists = $article->exists;
         $this->article = $article;
 
-        $this->id = $article->id;
-        if ($this->id)
-        {  
-            $this->title = $article->title;
-            $this->slug = $article->slug;
+        if ($this->exists)
+        {
+            $this->fill($article->only(["slug"]));
+            $this->fill(LocalesHelper::transformToProperties($article->getTranslationsArray()));
         }
     }
 
     public function render()
     {
-        return $this->view()->title(($this->id ? 'Update' : 'Create') . ' Article');
+        return $this->view()->title(($this->exists ? 'Update' : 'Create') . ' Article');
     }
 
     public function save(): void
     {
-        $fields = $this->validate();
+        $fields = LocalesHelper::transformToModelFields($this->validate(), $this->article->translatedAttributes);
         $this->article->fill($fields);
         $this->article->save();
     }
-};
-?>
+}; ?>
+
+@push('scripts')
+    @vite(['resources/js/vendor/tinymce.js'])
+@endpush
 
 <div>
-    <x-header :title="__('News & Announcement')" :subtitle="($id ? 'Update' : 'Create') . ' Article'" separator>
-        <x-slot:middle class="!justify-end max-md:hidden">
-            <x-input icon="fal.magnifying-glass" wire:model.live.debounce="searchQuery" type="search" :placeholder="__('Search...')" />
-        </x-slot:middle>
-        <x-slot:actions>
-            <x-button :label="__('Filters')" icon="fal.filter" @click="$wire.isDrawerOpened = true" responsive />
-            <x-button icon="fal.plus" class="btn-primary" :link="route('dashboard.news.create')" responsive />
-        </x-slot:actions>
+    <x-header :title="__('News & Announcement')" :subtitle="($exists ? 'Update' : 'Create') . ' Article'" separator>
     </x-header>
 
     <x-card shadow>
         <x-form wire:submit="save">
-            <x-input label="Title" wire:model="title" />
-            <x-input label="Slug" wire:model="slug" prefix="/news/" />
+            <x-tabs wire:model="selectedLanguage">
+                @foreach (Language::values() as $language)
+                    <x-tab wire:key="{{ $language }}" :name="$language" :label="__('languages.' . $language)">
+                        <x-input label="Title" wire:model="title.{{ $language }}" />
+                        <x-input label="Slug" wire:model="slug" prefix="/news/" />
+                        <x-editor label="Content" wire:model="content.{{ $language }}" gplLicense />
+                    </x-tab>
+                @endforeach
+            </x-tabs>
 
             <x-slot:actions>
                 <x-button label="Cancel" />
-                <x-button :label="($id ? 'Save' : 'Create')" :icon="'fal.' . ($id ? 'floppy-disk' : 'plus')" type="submit" class="btn-primary" spinner="save" />
+                <x-button :label="($exists ? 'Save' : 'Create')" :icon="'fal.' . ($exists ? 'floppy-disk' : 'plus')" type="submit" class="btn-primary" spinner="save" />
             </x-slot:actions>
         </x-form>
     </x-card>
