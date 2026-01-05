07708c4fb6bcc4a045fc656af36b28229653a167
Jason Kwok <git@jasonhk.dev>
2026-01-05 16:19:04 +0000

    Fix formatting inconsistencies in base layout file

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+<html lang="{{ Str::replace('_', '-', app()->getLocale()) }}" >
 <head>
     <meta charset="UTF-8">
-    <title>{{ isset($title) ? $title . " :: " . config("app.name") : config("app.name") }}</title>
+    <title>{{ isset($title) ? $title . ' :: ' . config('app.name') : config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="preload" href="/logo.svg" as="image" media="(prefers-color-scheme: light)" />
     <link rel="preload" href="/logo-dark.svg" as="image" media="(prefers-color-scheme: dark)" />
-    @vite(["resources/css/app.css", "resources/js/app.js"])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('scripts')
 </head>
 <body class="min-h-screen font-sans antialiased bg-base-200">
-    @yield("content")
+    @yield('content')
 
     <x-toast />
 </body>
