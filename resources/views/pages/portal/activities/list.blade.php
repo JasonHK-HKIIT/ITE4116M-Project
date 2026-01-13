c11ac1bfc33a2ee953e5d00f1e7116fabe849a21
Ng Lai Sang Stella <ng0812ji@gmail.com>
2026-01-13 18:19:04 +0000

    add activity date filter and sorting function

--- resources/views/pages/portal/activities/list.blade.php
+++ resources/views/pages/portal/activities/list.blade.php
@@ -15,13 +15,11 @@ class extends Component
 
     public string $keywords = '';
 
+    public array $sortBy = ['column' => 'title', 'direction' => 'desc'];
 
-    //filters
-    public ?string $type = null;
+
     public ?string $execution_from = null;
     public ?string $execution_to = null;
-    public ?string $attribute = null;
-    public bool $vacancy = false;
 
 
     public function clear()
@@ -29,15 +27,44 @@ class extends Component
         $this->reset([
             'keywords',
             'execution_from',
-            'execution_to'
+            'execution_to',
         ]);
     }
 
     public function activities()
     {
+
+
         return Activity::query()
             ->when($this->keywords, function ($query, $keywords) {
                 $query->whereFullText(['title', 'instructor', 'activity_code'], $keywords);
+            })
+            ->when($this->execution_from && $this->execution_to, function ($query) {
+                $from = $this->execution_from;
+                $to   = $this->execution_to;
+
+                $query->where('execution_from', '>=', $from)
+                    ->where('execution_to', '<=', $to);
+            })
+            ->when((($this->sortBy['column'] == 'execution_from') ? $this->sortBy : false), function ($query, $sortBy)
+            {
+                $query->orderBy('execution_from', ($sortBy['direction'] == 'asc') ? 'desc' : 'asc');
+            })
+            ->when((($this->sortBy['column'] == 'execution_to') ? $this->sortBy : false), function ($query, $sortBy)
+            {
+                $query->orderBy('execution_to', ($sortBy['direction'] == 'asc') ? 'desc' : 'asc');
+            })
+            ->when((($this->sortBy['column'] == 'instructor') ? $this->sortBy : false), function ($query, $sortBy)
+            {
+                $query->orderBy('instructor', ($sortBy['direction'] == 'asc') ? 'desc' : 'asc');
+            })
+            ->when((($this->sortBy['column'] == 'title') ? $this->sortBy : false), function ($query, $sortBy)
+            {
+                $query->orderBy('title', ($sortBy['direction'] == 'asc') ? 'desc' : 'asc');
+            })
+            ->when((($this->sortBy['column'] == 'total_amount') ? $this->sortBy : false), function ($query, $sortBy)
+            {
+                $query->orderBy('total_amount', ($sortBy['direction'] == 'asc') ? 'desc' : 'asc');
             })
             ->paginate($this->perPage);
     }
@@ -48,9 +75,9 @@ class extends Component
         return [
             ['key' => 'title', 'label' => 'Title','class' => 'w-auto min-w-64'],
             ['key' => 'instructor', 'label' => 'Instructor','class' => 'w-fit'],
-            ['key' => 'activity_type', 'label' => 'Activity type','class' => 'w-fit'],
+            ['key' => 'execution_from', 'label' => 'Execution From', 'class' => 'w-fit'],
+            ['key' => 'execution_to', 'label' => 'Execution To','class' => 'w-fit'],
             ['key' => 'total_amount', 'label' => 'Total amount','class' => 'w-fit'],
-            ['key' => 'has_vacancy', 'label' => 'Vacancy', 'class' => 'w-fit', 'format' => fn($activities, $has_vacancy) => $has_vacancy ? 'Yes' : 'No', 'sortable' => false],
         ];
     }
 
@@ -87,14 +114,12 @@ class extends Component
         <x-datepicker label="Execution From" wire:model.live="execution_from" clearable />
 
         <x-datepicker label="Execution to" wire:model.live="execution_to" clearable />
-        
-        <x-checkbox label="Activity with Vacancies only" wire:model="vacancy" right />
-        
+
     </div>
  
     <x-slot:actions>
         <x-button label="Reset" icon="fal.xmark" wire:click="clear" spinner />
-        <x-button label="Done" icon="fal.check" class="btn-primary" @click="$wire.isDrawerOpened = false" />
+        <x-button label="Done" class="btn-primary" @click="$wire.isDrawerOpened = false" />
     </x-slot:actions>
 
 
@@ -111,9 +136,8 @@ class extends Component
 
     </x-header>
 
-
     <x-card shadow>
-        <x-table :headers="$headers" :rows="$activities" with-pagination per-page="perPage" :per-page-values="[5, 10, 20]">
+        <x-table :headers="$headers" :rows="$activities" :sort-by="$sortBy" with-pagination per-page="perPage" :per-page-values="[5, 10, 20]">
             @scope('actions', $activity)
                 <div class="hidden lg:inline-flex flex-row w-8 lg:w-17">
                     <x-button icon="fal.file-lines" :tooltip="__('Activity Details')" :link="route('portal.activities.show', ['id' => $activity->id ])" class="btn-ghost btn-square btn-sm" />
