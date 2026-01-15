db8dc923598db6e5285b629c882214c1cd056a04
Jason Kwok <git@jasonhk.dev>
2026-01-15 13:29:40 +0000

    Add Blade component for brand and update logo references in layouts

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,8 +5,6 @@
     <title>{{ isset($title) ? $title . ' :: ' . config('app.name') : config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="preload" href="/logo.svg" as="image" media="(prefers-color-scheme: light)" />
-    <link rel="preload" href="/logo-dark.svg" as="image" media="(prefers-color-scheme: dark)" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('scripts')
 </head>
