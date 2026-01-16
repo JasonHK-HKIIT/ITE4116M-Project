d54d371a204402e304655e094d9a2e8828c34f4c
Jason Kwok <git@jasonhk.dev>
2026-01-16 10:27:48 +0000

    Add assistant configuration with base URL setting

--- config/app.php
+++ config/app.php
@@ -123,4 +123,8 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'assistant' => [
+        'base_url' => env('APP_ASSISTANT_BASE_URL', 'http://localhost:8100'),
+    ],
+
 ];
