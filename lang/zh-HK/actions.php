4291b179bf8214c8b74cf26062287ff73a06b9a5
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2026-04-07 11:52:49 +0000

    Added some translations for Traditional Chinese

---
+++ lang/zh-HK/actions.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'cancel' => '取消',
+    'create' => '建立',
+    'delete' => '刪除',
+    'edit' => '修改',
+    'save' => '儲存',
+];
---
+++ lang/zh-HK/appearance.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => '外觀',
+    'subtitle' => '自訂你的 MyPortal',
+    'language' => '語言',
+    'theme' => '主題',
+    'theme-auto' => '跟隨系統',
+    'theme-light' => '淺色',
+    'theme-dark' => '深色',
+];
